3dc133e15b15e6c6ef8a1dc48fb4eeceedcc811c
GM2SC <48650188+GM2SC@users.noreply.github.com>
2019-06-20 13:05:46 -0300

    Add files via upload

--- IFPA_CASTANHAL/valor_otimo.py
+++ IFPA_CASTANHAL/valor_otimo.py
@@ -19,6 +19,8 @@ Tema:
        EM FUNÇÃO DA TAXA DE DETERIORAÇÃO DE PRODUTOS  
        
 Disponível em:
+    https://github.com/GM2SC/DEVELOPMENT-OF-MATHEMATICAL-METHODS-IN-
+    COMPUTATIONAL-ENVIRONMENT/blob/master/IFPA_CASTANHAL/valor_otimo.py
     
 """
 
