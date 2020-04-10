8650908b9270f17b650b61419e75d98e37f21b81
GM2SC <48650188+GM2SC@users.noreply.github.com>
2020-04-10 16:02:35 -0300

    Add files via upload

--- SENGI_2020/Fourier_01.py
+++ SENGI_2020/Fourier_01.py
@@ -7,9 +7,10 @@ Created on Thu Feb 21 19:47:27 2019
         MSc. Lair A. de Meneses
         
 @authors Students:
-        Heictor A. de O. Costa
-        Brennus Caio
-        Júlio Azancort
+        Heictor Alves de Oliveira Costa
+        Lucas Pompeu Neves
+        Brennus Caio Carvalho da Cruz
+        Júlio Leite Azancort Neto
 
 Grupo de Pesquisa: 
                  Gradiente de Modelagem Matemática e
@@ -20,7 +21,8 @@ Assunto:
        
 Disponível em:
     
-   https://github.com/GM2SC/DEVELOPMENT-OF-MATHEMATICAL-METHODS-IN-COMPUTATIONAL-ENVIRONMENT/blob/master/SENGI_2020/Fourier_01.py
+    https://github.com/GM2SC/DEVELOPMENT-OF-MATHEMATICAL-METHODS-IN-COMPUTATIONAL-ENVIRONMENT/blob/master/SENGI_2020/Fourier_01.py
+    
 """
 # Importando Bibliotecas
 # Biblioteca numpy: Matemática de alto Nível
@@ -32,25 +34,30 @@ Disponível em:
 
 # Biblioteca sympy: Matemática Simbólica
 import sympy as sy
+import numpy as np
 
 ##########################################################
 print('=================================================')
 # Variáveis de Controle
 
-T0 = 0                 # Instante Inicial
+T0 = 0.0               # Instante Inicial
 
-T1 = 5                 # Instante Final
+T1 = 10                # Instante Final
 
 T = T1 - T0            # Período Fundamenta
 
 w = 2*sy.pi/T          # Frequência Angular
 
+
+t = np.linspace(T0,T1,100)
+
 lista = []
 
-n = int(input('Entre com o número do harmônico n --> ')  ) 
+n = int(input('>>> Entre com o número do harmônico n --> ')  ) 
 # Número de Harmônicos
 print('')
-print('Número de Harmônicos =', n)
+print('Número de Harmônicos >>>', n)
+print('=================================================')
 S0 = 0
 for i in range(1, n + 1):
     
@@ -59,11 +66,10 @@ for i in range(1, n + 1):
     # Declarando a variável simbólica
     t = sy.symbols('t')
 
-
     # Declarando a Função - Sinal
     def f(t):
-        return 2 * t
-
+        return 10*t-t**2
+      
     # Declarando a amplitude Real do Sinal
     def a0(t):
         return (2/T)*sy.integrate(f(t), (t, T0, T1))
@@ -78,7 +84,7 @@ for i in range(1, n + 1):
     def bn(t):
         return (2/T)*sy.integrate(f(t) * sy.sin(n * w * t), (t, T0, T1))
     print('bn =', bn(t))
-    print('=================================================')
+    #print('=================================================')
 
     ##########################################################
     # Compondo a Função Resposta do Sinal
@@ -89,7 +95,7 @@ for i in range(1, n + 1):
     
     print('Fn(t) é o Somatório no n-ésimo Harmônico')
     
-    print("Função para {} harmonico(s):".format(i), Fn(t))
+    print("Função para {} harmônico(s):".format(i), Fn(t))
     
     S0 = S0 + Fn(t)
 
@@ -97,13 +103,14 @@ for i in range(1, n + 1):
     print('')
     print('F(n) = Σ[an.cos(nwt) + bn.sen(nwt)]')
     print('')
+    print('=================================================')
     print('Para n = ', n, '--> F(n) =', Fn(t))
     print('')
 print("S =", 0.5*a0(t) + S0)
 
 ##########################################################
 print(' ')
-print(' ===== Fim do Programa Fourier_01 ====')
+print(' =========== Fim do Programa Fourier_01 ==========')
 
 
 
